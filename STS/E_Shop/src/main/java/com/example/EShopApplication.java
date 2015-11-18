3cbfe00ee3fb5cacec0b166b50898acfd6b5cd14
Kuznetsov Sergey <azon.sk@gmail.com>
2015-11-18 16:59:18 +0200

    develop start

--- STS/E_Shop/src/main/java/com/example/EShopApplication.java
+++ STS/E_Shop/src/main/java/com/example/EShopApplication.java
@@ -7,6 +7,5 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class EShopApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(EShopApplication.class, args);
-		new TimeReader();
 	}
 }
---
+++ STS/E_Shop/src/main/java/com/example/MinuteReader.java
@@ -0,0 +1,7 @@
+package com.example;
+
+public class MinuteReader {
+	public String outputColor() {
+		return "Green";
+	}
+}
