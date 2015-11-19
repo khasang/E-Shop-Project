b4c691d097451e72d89c3873856deaf5e21a8941
AlexanderGal <Balron923@gmail.com>
2015-11-19 07:04:26 +0300

    Add new functionality

--- STS/E_Shop/src/main/java/com/eshop/MinuteReader.java
+++ STS/E_Shop/src/main/java/com/eshop/MinuteReader.java
@@ -1,7 +1,40 @@
 package com.eshop;
 
+import java.util.Scanner;
+
 public class MinuteReader {
-	public String outputColor() {
-		return "Green";
+	private Scanner inputFromUser;
+	private int userInputNumber;
+
+	public MinuteReader() {
+		minuteReaderCyclie();
+	}
+	
+	public void minuteReaderCyclie(){
+		while (true) {
+			if (isNumberRangeTrue(userInputNumber)){
+				break;
+			}
+		}
+	}
+
+	public void readNumberFromConsole() {
+		try {
+			inputFromUser = new Scanner(System.in);
+			userInputNumber = Integer.parseInt(inputFromUser.nextLine());
+		} catch (Exception e) {
+			System.out.println("Wrong format");
+		}
+	}
+
+	public boolean isNumberRangeTrue(int numberToCheck) {
+		if (numberToCheck > 0 && numberToCheck < 59) {
+			return true;
+		}
+		return false;
+	}
+
+	public int getUserInputNumber(){
+		return userInputNumber;
 	}
 }
