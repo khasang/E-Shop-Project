11e3098e833904e4c2569cb8d084a17a9711c4e1
andrsam <andrsbox@gmail.com>
2015-11-20 22:01:27 +0500

    1)Изменены классы TimeReader и LightTimer 2)Удален класс MinuteReader 3)Создан класс Messages

---
+++ STS/E_Shop/src/main/java/com/eshop/Messages.java
@@ -0,0 +1,10 @@
+package com.eshop;
+
+public class Messages {
+  final static String RED = "Red",
+		              YELLOW = "Yellow",
+               		  GREEN ="Green",
+               		  ENTER_NUMBER = "Enter a number from 0 to 59: ",
+               	      REENTER_NUMBER = "Re-enter a number from 0 to 59: ",
+               	      CHECK_LOCALHOST = "Check localhost:8080/result";
+}
--- STS/E_Shop/src/main/java/com/eshop/TimeReader.java
+++ STS/E_Shop/src/main/java/com/eshop/TimeReader.java
@@ -2,28 +2,19 @@ package com.eshop;
 
 import java.util.Scanner;
 
-abstract public class TimeReader {
+abstract public class TimeReader {	
 	private static int minute;
-	Scanner inputInfoMinutesFromUser;
-
+	
 	public static int getMinute() {
+		try(Scanner inputMinutes = new Scanner(System.in)) {
+			System.out.println(Messages.ENTER_NUMBER);		
+			minute = inputMinutes.nextInt();
+			while (minute > 59 || minute < 0) {
+				System.out.println(Messages.REENTER_NUMBER);
+				minute = inputMinutes.nextInt();
+			}			
+			System.out.println(Messages.CHECK_LOCALHOST);
+		}
 		return minute;
-	}
-
-	void resultColorOut() {
-		MinuteReader reader = new MinuteReader();
-		System.out.println(reader.outputColor());
-	}
-	/*
-	 * try { inputInfoMinutesFromUser = new Scanner(System.in);
-	 * System.out.println("Enter a number from 0 to 59"); minute =
-	 * Integer.parseInt(inputInfoMinutesFromUser.nextLine()); while (minute > 59
-	 * || minute < 0) { System.out.println("Re-enter a number from 0 to 59");
-	 * minute = Integer.parseInt(inputInfoMinutesFromUser.nextLine()); }
-	 * System.out.println("Check localhost:8080/result"); } catch
-	 * (NumberFormatException e) { System.err.println("Wrong format!"); }
-	 * finally { if (inputInfoMinutesFromUser != null)
-	 * inputInfoMinutesFromUser.close(); }
-	 */
-
+	} 
 }
