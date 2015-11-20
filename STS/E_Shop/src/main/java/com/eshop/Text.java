953df2348332c44a16ee61d38b55b3d0276e5001
AlexanderGal <Balron923@gmail.com>
2015-11-20 20:57:30 +0300

    fix

--- STS/E_Shop/src/main/java/com/eshop/Text.java
+++ STS/E_Shop/src/main/java/com/eshop/Text.java
@@ -1,12 +1,6 @@
 package com.eshop;
 
 public class Text {
-	public static final String RED = "Red";
-	public static final String YELLOW = "Yellow";
-	public static final String GREEN = "Green";
-	public static final String ENTER_NUMBER = "Enter the number of minute";
-	public static final String WRONG_FORMAT = "Wrong format";
-	public static final String RESULT_URL = "Check localhost:8080/result";
-
-
+	final String RED = "Red", YELLOW = "Yellow", GREEN = "Green", ENTER_NUMBER = "Enter the number of minute",
+			WRONG_FORMAT = "Wrong format", RESULT_URL = "Check localhost:8080/result";
 }
