cff6b56cecf30853bb2cd2aafef1621179a25335
AlexanderGal <Balron923@gmail.com>
2015-12-15 23:10:05 +0300

    small fix

--- src/com/eshop/controller/AppController.java
+++ src/com/eshop/controller/AppController.java
@@ -1,15 +1,12 @@
 package com.eshop.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
-import com.eshop.model.BackupDB;
-import com.eshop.model.InsertDataTable;
-import com.eshop.model.SelectDataTable;
-import com.eshop.model.DeleteDataTable;
-import com.eshop.model.CreateDataTable;
-import com.eshop.model.ShrinkDataDB;
+import com.eshop.model.*;
 
 @Controller
 public class AppController {
@@ -22,17 +19,36 @@ public class AppController {
 	@Autowired
 	SelectDataTable selectDataTable;
 
-	@RequestMapping("/")
-	public ModelAndView inputForm() {
-		ModelAndView modelandview = new ModelAndView("E-Shop");
-		modelandview.addObject("msg", "You can buy different things at our shop!");
+	@RequestMapping("orderslist")
+	public ModelAndView orderListView() {
+		ModelAndView modelandview = new ModelAndView("orders");
+		modelandview.addObject("list", selectDataTable.viewTable("product"));
+		modelandview.addObject("tableTitleList", selectDataTable.getTableColumnName("product"));
 		return modelandview;
 	}
 
-	@RequestMapping("webshop/welcome")
-	public ModelAndView hello() {
+	@RequestMapping(value = "deleteOrder", method = RequestMethod.POST)
+	public ModelAndView orderListDeleteItem(@RequestParam(value = "orderName") String selectedOrderId) {
+		deleteDataTable.deleteDataFromTable(selectedOrderId);
+		ModelAndView modelandview = new ModelAndView("orders");
+		modelandview.addObject("list", selectDataTable.viewTable("product"));
+		modelandview.addObject("tableTitleList", selectDataTable.getTableColumnName("product"));
+		return modelandview;
+	}
+
+	@RequestMapping(value = "insertOrder", method = RequestMethod.POST)
+	public ModelAndView orderListAddOrder(@RequestParam(value = "orderName") String orderName) {
+		insertDataTable.insertDataInTable(orderName);
+		ModelAndView modelandview = new ModelAndView("orders");
+		modelandview.addObject("list", selectDataTable.viewTable("product"));
+		modelandview.addObject("tableTitleList", selectDataTable.getTableColumnName("product"));
+		return modelandview;
+	}
+
+	@RequestMapping("/")
+	public ModelAndView inputForm() {
 		ModelAndView modelandview = new ModelAndView("E-Shop");
-		modelandview.addObject("msg", "You can buy different things at our shop!");
+		modelandview.addObject("result", "Welcome to our Eshop project!");
 		return modelandview;
 	}
 
@@ -49,51 +65,46 @@ public class AppController {
 		return modelandview;
 	}
 
-	@RequestMapping("webshop/CreateDataTable")
-	public ModelAndView createDataTable() {
-		ModelAndView modelandview = new ModelAndView("E-Shop");
-		modelandview.addObject("msg", CreateDataTable.class);
+	@RequestMapping("deletetable")
+	public String deleteTableView() {
+		return "deletetable";
+	}
+
+	@RequestMapping("deleteDataTable")
+	public ModelAndView deleteDataTable(@RequestParam(value = "tableName") String tableName) {
+		ModelAndView modelandview = new ModelAndView("deletetable");
+		deleteDataTable.dropDataTable(tableName);
+		modelandview.addObject("result", deleteDataTable.getResult());
 		return modelandview;
 	}
 
 	@RequestMapping("insertdata")
 	public ModelAndView insertDataView() {
 		ModelAndView modelandview = new ModelAndView("insertdata");
-		DeleteDataTable deleteDataTable = DeleteDataTable.getInstance();
-		deleteDataTable.deleteTable("trafficlight");
-		modelandview.addObject("msg", deleteDataTable.getResultMessage());
+		modelandview.addObject("tableTitleList", selectDataTable.dataBaseAllTableName());
 		return modelandview;
 	}
 
 	@RequestMapping("insertDataTable")
-	public ModelAndView insertDataTable(@RequestParam(value = "tableName") String tableName,
+	public ModelAndView insertDataTable(@RequestParam(value = "tableTitleList") String tableName,
 			@RequestParam(value = "name") String name, @RequestParam(value = "amount") String amount) {
 		ModelAndView modelandview = new ModelAndView("insertdata");
 		insertDataTable.insertDataInTable(tableName, name, amount);
+		modelandview.addObject("tableTitleList", selectDataTable.dataBaseAllTableName());
 		modelandview.addObject("result", insertDataTable.getResult());
 		return modelandview;
 	}
 
 	@RequestMapping("selectdata")
 	public ModelAndView selectDataView() {
-		ModelAndView modelandview = new ModelAndView("E-Shop");
-		InsertDataTable insertData = InsertDataTable.getInstance();
-		insertData.insertDataInTable(name, amount);
-		modelandview.addObject("msg", insertData.getInsertResult());
+		ModelAndView modelandview = new ModelAndView("selectdatatable");
 		return modelandview;
 	}
 
 	@RequestMapping("selectDataTable")
-	public ModelAndView selectDataTable() {
-		ModelAndView modelandview = new ModelAndView("SelectDataTable");
-		modelandview.addObject("list", new SelectDataTable().viewTable());
-		return modelandview;
-	}
-
-	@RequestMapping("webshop/ShrinkDataDB")
-	public ModelAndView shrinkDataDB() throws SQLException{
-		ModelAndView modelandview = new ModelAndView("E-Shop");
-		modelandview.addObject("msg", new ShrinkDataDB().shrinkDataDB());
+	public ModelAndView selectDataTable(@RequestParam(value = "tableName") String tableName) {
+		ModelAndView modelandview = new ModelAndView("selectdatatable");
+		modelandview.addObject("list", selectDataTable.viewTable(tableName));
 		return modelandview;
 	}
 }
