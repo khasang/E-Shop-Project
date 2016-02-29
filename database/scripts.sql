e0fe66c705397eefc9048d09638b93b9f61c643b
andrsam <andrsbox@gmail.com>
2016-02-29 23:39:46 +0500

    add categories table

--- database/scripts.sql
+++ database/scripts.sql
@@ -29,3 +29,10 @@ CREATE TABLE `status` (
   `name` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+
+CREATE TABLE `category` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+SELECT * FROM eshop.product;
